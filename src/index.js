e1aaf85cf8e3ac94f70a2a91f7b27d4a9fba3f27
Quinn <wegnerq@gmail.com>
2025-05-07 09:31:02 -0600

    12

--- src/index.js
+++ src/index.js
@@ -788,43 +788,6 @@ export default class Gantt {
         return { left, dateObj };
     }
 
-    play_animated_highlight(left, dateObj) {
-        if (!left || !dateObj) return;
-        this.$animated_highlight = this.create_el({
-            top: this.config.header_height,
-            left,
-            height: this.grid_height - this.config.header_height,
-            classes: 'animated-highlight',
-            append_to: this.$container,
-            style: 'fill: #ff0000;',
-        });
-        this.$animated_ball_highlight = this.create_el({
-            top: this.config.header_height - 6,
-            left: left - 2.5,
-            width: 6,
-            height: 6,
-            classes: 'animated-ball-highlight',
-            append_to: this.$header,
-            style: 'fill: #ff0000;',
-        });
-        this.$animated_highlight.style.animation =
-            'highlight 1s linear infinite';
-        this.$animated_ball_highlight.style.animation =
-            'highlight 1s linear infinite';
-        this.$animated_highlight.style.animationPlayState = 'running';
-        this.$animated_ball_highlight.style.animationPlayState = 'running';
-        this.$animated_highlight.style.animationDirection = 'normal';
-        this.$animated_ball_highlight.style.animationDirection = 'normal';
-        this.$animated_highlight.style.animationIterationCount = 'infinite';
-        this.$animated_ball_highlight.style.animationIterationCount =
-            'infinite';
-
-        return {
-            left,
-            dateObj,
-        };
-    }
-
     make_grid_highlights() {
         this.highlight_holidays();
         this.config.ignored_positions = [];
@@ -878,16 +841,16 @@ export default class Gantt {
             );
         }
         if (!highlightDimensions || !highlightDimensionsCustom) return;
-        // if (this.options.player_state) {
-        //     try {
-        //         this.play_animated_highlight(
-        //             highlightDimensionsCustom.left,
-        //             highlightDimensionsCustom.dateObj,
-        //         );
-        //     } catch (e) {
-        //         console.error('Error playing animated highlight:', e);
-        //     }
-        // }
+        if (this.options.player_state) {
+            try {
+                this.play_animated_highlight(
+                    highlightDimensionsCustom.left,
+                    highlightDimensionsCustom.dateObj,
+                );
+            } catch (e) {
+                console.error('Error playing animated highlight:', e);
+            }
+        }
     }
 
     create_el({ left, top, width, height, id, classes, append_to, type }) {
@@ -1854,6 +1817,43 @@ export default class Gantt {
         this.$animated_ball_highlight?.remove?.();
         this.$extras?.remove?.();
         this.popup?.hide?.();
+    }
+
+    play_animated_highlight(left, dateObj) {
+        if (!left || !dateObj) return null;
+        this.$animated_highlight = this.create_el({
+            top: this.config.header_height,
+            left,
+            height: this.grid_height - this.config.header_height,
+            classes: 'animated-highlight',
+            append_to: this.$container,
+            style: 'fill: #ff0000;',
+        });
+        this.$animated_ball_highlight = this.create_el({
+            top: this.config.header_height - 6,
+            left: left - 2.5,
+            width: 6,
+            height: 6,
+            classes: 'animated-ball-highlight',
+            append_to: this.$header,
+            style: 'fill: #ff0000;',
+        });
+        this.$animated_highlight.style.animation =
+            'highlight 1s linear infinite';
+        this.$animated_ball_highlight.style.animation =
+            'highlight 1s linear infinite';
+        this.$animated_highlight.style.animationPlayState = 'running';
+        this.$animated_ball_highlight.style.animationPlayState = 'running';
+        this.$animated_highlight.style.animationDirection = 'normal';
+        this.$animated_ball_highlight.style.animationDirection = 'normal';
+        this.$animated_highlight.style.animationIterationCount = 'infinite';
+        this.$animated_ball_highlight.style.animationIterationCount =
+            'infinite';
+
+        return {
+            left,
+            dateObj,
+        };
     }
 }
 
