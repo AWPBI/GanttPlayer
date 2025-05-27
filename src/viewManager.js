1d17a7e6dffcfdea4e5387dfd8f24886130c7c52
Quinn <wegnerq@gmail.com>
2025-05-26 18:11:57 -0600

    date setup 7 reduce start

--- src/viewManager.js
+++ src/viewManager.js
@@ -50,14 +50,6 @@ export default class ViewManager {
             this.gantt.options.lower_header_height +
             this.gantt.options.upper_header_height +
             10;
-        console.log(
-            'update_view_scale: column_width=',
-            this.gantt.config.column_width,
-            'step=',
-            this.gantt.config.step,
-            'unit=',
-            this.gantt.config.unit,
-        );
     }
 
     setup_dates(refresh = false) {
@@ -66,25 +58,10 @@ export default class ViewManager {
     }
 
     setup_gantt_dates(refresh) {
-        console.log(
-            'setup_gantt_dates: view_mode=',
-            this.gantt.options.view_mode,
-            'refresh=',
-            refresh,
-            'infinite_padding=',
-            this.gantt.options.infinite_padding,
-            'padding=',
-            this.gantt.config.view_mode.padding,
-        );
         let gantt_start, gantt_end;
         if (!this.gantt.tasks.length) {
             gantt_start = date_utils.today();
             gantt_end = date_utils.add(date_utils.today(), 1, 'year');
-            console.log(
-                'setup_gantt_dates: no tasks, using today:',
-                gantt_start,
-                gantt_end,
-            );
         } else {
             gantt_start = this.gantt.tasks[0]._start;
             gantt_end = this.gantt.tasks[0]._end;
@@ -96,20 +73,10 @@ export default class ViewManager {
                     gantt_end = task._end;
                 }
             }
-            console.log(
-                'setup_gantt_dates: task range:',
-                gantt_start,
-                gantt_end,
-            );
         }
 
         gantt_start = date_utils.start_of(gantt_start, this.gantt.config.unit);
         gantt_end = date_utils.start_of(gantt_end, this.gantt.config.unit);
-        console.log(
-            'setup_gantt_dates: after start_of:',
-            gantt_start,
-            gantt_end,
-        );
 
         if (!this.gantt.options.infinite_padding) {
             if (typeof this.gantt.config.view_mode.padding === 'string') {
@@ -126,12 +93,6 @@ export default class ViewManager {
                     { duration: 1, scale: this.gantt.config.unit },
                     { duration: 1, scale: this.gantt.config.unit },
                 ];
-            console.log(
-                'setup_gantt_dates: padding:',
-                padding_start,
-                padding_end,
-            );
-
             this.gantt.gantt_start = date_utils.add(
                 gantt_start,
                 -padding_start.duration,
@@ -142,16 +103,11 @@ export default class ViewManager {
                 padding_end.duration,
                 padding_end.scale,
             );
-            console.log(
-                'setup_gantt_dates: after view_mode.padding:',
-                this.gantt.gantt_start,
-                this.gantt.gantt_end,
-            );
         } else {
             const extend_units = this.gantt.config.extend_by_units * 3;
             this.gantt.gantt_start = date_utils.add(
                 gantt_start,
-                -extend_units,
+                1,
                 this.gantt.config.unit,
             );
             this.gantt.gantt_end = date_utils.add(
@@ -159,14 +115,7 @@ export default class ViewManager {
                 extend_units,
                 this.gantt.config.unit,
             );
-            console.log(
-                'setup_gantt_dates: after infinite_padding:',
-                this.gantt.gantt_start,
-                this.gantt.gantt_end,
-            );
         }
-
-        // Always add extra future padding for Month and Year modes
         if (this.gantt.options.view_mode === 'Year') {
             this.gantt.gantt_end = date_utils.add(
                 this.gantt.gantt_end,
@@ -180,22 +129,11 @@ export default class ViewManager {
                 'month',
             );
         }
-        console.log(
-            'setup_gantt_dates: after extra padding:',
-            this.gantt.gantt_start,
-            this.gantt.gantt_end,
-        );
-
         this.gantt.config.date_format =
             this.gantt.config.view_mode.date_format ||
             this.gantt.options.date_format;
         this.gantt.gantt_start.setHours(0, 0, 0, 0);
         this.gantt.gantt_end.setHours(0, 0, 0, 0);
-        console.log(
-            'setup_gantt_dates: final:',
-            this.gantt.gantt_start,
-            this.gantt.gantt_end,
-        );
     }
 
     setup_date_values() {
